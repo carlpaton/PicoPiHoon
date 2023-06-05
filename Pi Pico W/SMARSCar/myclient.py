b0b94143546d636e85b0b10b6257efbdb8372829
Carl <carl.paton@gmail.com>
2023-06-05 13:32:19 +1200

    updated ui and control in client

--- Pi Pico W/SMARSCar/myclient.py
+++ Pi Pico W/SMARSCar/myclient.py
@@ -1,6 +1,9 @@
 from machine import Pin
 
-led = Pin(15, Pin.OUT)
+motor_a_fw = Pin(18, Pin.OUT)
+motor_a_bk = Pin(19, Pin.OUT)
+motor_b_fw = Pin(20, Pin.OUT)
+motor_b_bk = Pin(21, Pin.OUT)
 
 def read_html():
     text_file = open("html.htm", "r")
@@ -8,7 +11,43 @@ def read_html():
     text_file.close()
     return data
 
+def move_forward():
+    print("moving forward")
+    motor_a_fw.value(1)
+    motor_b_fw.value(1)
+    motor_a_bk.value(0)
+    motor_b_bk.value(0)
+    
+def move_left():
+    print("moving left")
+    motor_a_fw.value(1)
+    motor_b_fw.value(0)
+    motor_a_bk.value(0)
+    motor_b_bk.value(1)
+
+def stop_motors():
+    print("stop motors")
+    motor_a_fw.value(0)
+    motor_b_fw.value(0)
+    motor_a_bk.value(0)
+    motor_b_bk.value(0)
+
+def move_right():
+    print("moving right")
+    motor_a_fw.value(0)
+    motor_b_fw.value(1)
+    motor_a_bk.value(1)
+    motor_b_bk.value(0)
+
+def move_back():
+    print("moving back")
+    motor_a_fw.value(0)
+    motor_b_fw.value(0)
+    motor_a_bk.value(1)
+    motor_b_bk.value(1)
+
 async def serve_client(reader, writer):
+    stop_motors()
     print("Client connected")
     request_line = await reader.readline()
     print("Request:", request_line)
@@ -18,18 +57,26 @@ async def serve_client(reader, writer):
         pass
 
     request = str(request_line)
-    led_on = request.find('/led/on')
-    led_off = request.find('/led/off')
-    print('led on = ' + str(led_on))
-    print('led off = ' + str(led_off))
+    forward = request.find('/motor/forward')
+    left = request.find('/motor/left')
+    stop = request.find('/motor/stop')
+    right = request.find('/motor/right')
+    back = request.find('/motor/back')
 
-    if led_on == 6:
-        print("led on")
-        led.value(1)
+    if forward == 6:
+        move_forward()
 
-    if led_off == 6:
-        print("led off")
-        led.value(0)
+    if left == 6:
+        move_left()
+
+    if stop == 6:
+        stop_motors()
+
+    if right == 6:
+        move_right()
+
+    if back == 6:
+        move_back()
 
     writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
     writer.write(read_html())
